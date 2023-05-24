9400dbb4721739cc3416468d2189097bd3da4b6b
Anthony Shull <shull.anthony@gmail.com>
2023-05-24 17:21:02 -0500

    remove websocketd references

--- processor.c
+++ processor.c
@@ -3,19 +3,19 @@
 static ERL_NIF_TERM
 avg(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
-    return enif_make_int(env, 1);
+    return enif_make_int(env, 2);
 }
 
 static ERL_NIF_TERM
 max(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
-    return enif_make_int(env, 2);
+    return enif_make_int(env, 3);
 }
 
 static ERL_NIF_TERM
 min(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
-    return enif_make_int(env, 3);
+    return enif_make_int(env, 1);
 }
 
 static ErlNifFunc nif_funcs[] =
